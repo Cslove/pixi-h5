db1fae609c972ac3c8e44c2dbe396e9e7502e37f
cslove <969118892@qq.com>
2018-06-13 18:59:59 +0800

    webGl

--- js/index.js
+++ js/index.js
@@ -2,19 +2,19 @@ let Application = PIXI.Application,
 	Sprite = PIXI.Sprite,
 	Loader = PIXI.loader,
 	resources = PIXI.loader.resources;
-let app = new Application({forceCanvas:true});
+let app = new Application();
 	app.renderer.view.style.position = "absolute";
 	app.renderer.view.style.display = "block";
 	app.renderer.autoResize = true;
 	app.renderer.resize(window.innerWidth, window.innerHeight);
 	document.body.appendChild(app.view);
 
-Loader.add('./img/score.png').load(setup);
+Loader.add('./img/practice.json').load(setup);
 
 function setup(){
-	let id = PIXI.loader.resources["./img/score.png"].texture;
+	let id = PIXI.loader.resources["./img/practice.json"].textures;
 	
-	let sprite = new Sprite(id);
+	let sprite = new Sprite(id['bird0.png']);
 	sprite.x = 300;
 	sprite.y=500;
 	sprite.width = 200;
